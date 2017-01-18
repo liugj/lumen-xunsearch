aa5c4d0799f42020689221fb25b5ea2e2de3635d
qinjb <qinjb@boqii.com>
2017-01-18 02:17:14 +0000

    add use namespace

--- src/Searchable.php
+++ src/Searchable.php
@@ -4,6 +4,7 @@ namespace Liugj\Xunsearch;
 use Laravel\Scout\Searchable as ScoutSearchable;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Support\Collection as BaseCollection;
+use Liugj\Xunsearch\Jobs\MakeSearchable;
 
 trait Searchable
 {
